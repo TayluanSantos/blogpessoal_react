5843b318255d1542bfaa52bdb589ac714b68cea8
TayluanSantos <tayluanjesus@gmail.com>
2024-08-26 18:10:37 -0300

    feat: Criando e estilizando componentes Cadastro e Login e adicionando funcionalidades

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,28 @@ import Footer from './components/footer/Footer'
 import Navbar from './components/navbar/Navbar'
 import Home from './pages/home/Home'
 import Cadastro from './pages/cadastro/Cadastro'
+import Login from './pages/login/Login'
+import { AuthProvider } from './contexts/AuthContext'
 
 function App() {
   //Código Typescript
   return (
   // Código TSX - HTML e CSS
   <>
+  <AuthProvider>
     <BrowserRouter>
-        <Navbar/>
-        <div className="min-h-[80vh]">
-        <Routes>
-          <Route path = "/" element={<Home/>}/>
-          <Route path = "/home" element={<Home/>}/>
-        </Routes>
-        </div>
-        <Footer/>
-    </BrowserRouter>
+          <Navbar/>
+          <div className="min-h-[80vh]">
+          <Routes>
+            <Route path = "/" element={<Login/>}/>
+            <Route path = "/home" element={<Home/>}/>
+            <Route path = "/cadastro" element={<Cadastro/>}/>
+            <Route path = "/login" element={<Login/>}/>
+          </Routes>
+          </div>
+          <Footer/>
+      </BrowserRouter>
+  </AuthProvider>
   </>
   )
 }
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,18 +1,30 @@
-import { Link } from "react-router-dom"
+import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom"
+import AuthContext from "../../contexts/AuthContext";
 
 function Navbar() {
+  const navigate = useNavigate();
+  const {handleLogout} = useContext(AuthContext)
+  
+  function logout(){
+    handleLogout();
+    alert("Usuário foi desconectado com sucesso!");
+    navigate("/");
+  }
+
+
   return (
     <div className="flex justify-between  bg-indigo-900 px-20 py-3 text-white">
-        <div className="text-xl font-bold uppercase">
+        <div className="text-2xl font-bold uppercase">
             <Link to='/home'>Blog Pessoal</Link>
         </div>
         <div>
             <ul className="flex *:px-1">
-              <li>Postagem</li>
-              <li>Tema</li>
-              <li>Cadastrar Tema</li>
-              <li>Perfil</li>
-              <li>Sair</li>
+              <li className="hover:underline cursor-pointer">Postagem</li>
+              <li className="hover:underline cursor-pointer">Tema</li>
+              <li className="hover:underline cursor-pointer">Cadastrar Tema</li>
+              <li className="hover:underline cursor-pointer">Perfil</li>
+              <li className="hover:underline cursor-pointer"><Link to ='' onClick={logout}>Sair </Link></li>
             </ul>
         </div>
     </div>
